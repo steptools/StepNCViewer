16b269993e69ca59f3235456267c3b2c02e23f48
Will Rigby-Hall <will@wgrh.io>
2016-06-14 14:11:13 -0400

    Revert to b289718

--- src/server/api/v2/geometry.js
+++ src/server/api/v2/geometry.js
@@ -9,19 +9,22 @@ var _getGeometry = function(req , res){
     let ncId = req.params.ncId;
     var ms = file.getMachineState(ncId);
   }
+  
   if(req.params.type === "shell"){
-    console.log(ms.GetGeometryJSON(req.params.uuid , "POLYLINE"));
     res.status(200).send(ms.GetGeometryJSON(req.params.uuid , "MESH"));
-
   }
   else if(req.params.type === "annotation"){
-    console.log(ms.GetGeometryJSON(req.params.uuid , "POLYLINE"));
     res.status(200).send(ms.GetGeometryJSON(req.params.uuid , "POLYLINE"));
-
+  }
+  else {
+     let ret = '';
+     ret = ms.GetGeometryJSON();
+     res.status(200).send(ret);
   }
 }
 module.exports = function(app, cb) {
   app.router.get("/v2/nc/projects/:ncId/geometry", _getGeometry);
   app.router.get("/v2/nc/projects/:ncId/geometry/:uuid/:type", _getGeometry);
+  app.router.get("/v2/nc/projects/:ncId/geometry/:type/:uuid", _getGeometry);
   if (cb) cb();
 };

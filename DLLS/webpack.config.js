93d575b231a043b7431828fabf3a36363467a25d
Will Rigby-Hall <will@wgrh.io>
2016-08-09 10:56:34 -0400

    FOLLOW THE STYLEGUIDE STEVEN

--- DLLS/webpack.config.js
+++ DLLS/webpack.config.js
@@ -1,47 +1,46 @@
-var path                =      require("path"),
-    webpack             =   require("webpack"),
-    ExtractTextPlugin   = require("extract-text-webpack-plugin");
-module.exports = { 
+var webpack = require('webpack');
+
+module.exports = {
   context: process.cwd(),
   entry: {
     dependencies:[
-     'async',
-     'bootstrap',
-     'body-parser',
-     'commander',
-     'cookie-parser',
-     'errorhandler',
-     'express',
-     'express-session',
-     'jade',
-     'jquery',
-     'joi',
-     'jsonwebtoken',
-     'jstree',
-     'jwt-decode',
-     'lodash',
-     'method-override',
-     'morgan',
-     'node-sass',
-     'pug',
-     'qs',
-     'query-string',
-     'rc-menu',
-     'react', 
-     'react-dom', 
-     'react-tooltip',
-     'react-treebeard',
-     'request',
-     'serve-favicon',
-     'serve-static',
-     'socket.io',
-     'socket.io-client',
-     'socket.io-express-session',
-     'speakeasy',
-     'superagent',
-     'three',
-     'webpack-sources',
-     'winston'
+      'async',
+      'bootstrap',
+      'body-parser',
+      'commander',
+      'cookie-parser',
+      'errorhandler',
+      'express',
+      'express-session',
+      'jade',
+      'jquery',
+      'joi',
+      'jsonwebtoken',
+      'jstree',
+      'jwt-decode',
+      'lodash',
+      'method-override',
+      'morgan',
+      'node-sass',
+      'pug',
+      'qs',
+      'query-string',
+      'rc-menu',
+      'react',
+      'react-dom',
+      'react-tooltip',
+      'react-treebeard',
+      'request',
+      'serve-favicon',
+      'serve-static',
+      'socket.io',
+      'socket.io-client',
+      'socket.io-express-session',
+      'speakeasy',
+      'superagent',
+      'three',
+      'webpack-sources',
+      'winston',
     ],
     devdepends: [
       'babel',
@@ -67,21 +66,18 @@ module.exports = {
       'webpack-config',
       'style-loader',
       'sass-loader',
-      'less'
-
-    ]
-  }, 
- 
- output: { 
-    filename: '[name].dll.js', 
-    path: "", 
-    library: '[name]', 
-  }, 
-  
-  plugins: [ 
-    new webpack.DllPlugin({ 
-      name: '[name]', 
-      path:  '[name].json'
-    })
-  ]
+      'less',
+    ],
+  },
+  output: {
+    filename: '[name].dll.js',
+    path: '',
+    library: '[name]',
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      name: '[name]',
+      path: '[name].json',
+    }),
+  ],
 };

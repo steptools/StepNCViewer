f671ba8480d55db6b86753777f1270cb87408585
Samson <bonfante@steptools.com>
2016-09-28 13:31:03 -0400

    Heartbeat changed to 10 sec default.

--- src/server/api/v3/parseupdatethread.js
+++ src/server/api/v3/parseupdatethread.js
@@ -69,6 +69,6 @@ var updateLoop = function(data){
 onmessage =function(f){
     f=f.data;
     if(f.msg==='start'){
-        getMultipartRequest({'hostname':f.machineAddress,'port':f.machinePort,'path':'/sample?from='+f.startSequence+'&interval=0&heartbeat=1000'},(r)=>{updateLoop(r);});
+        getMultipartRequest({'hostname':f.machineAddress,'port':f.machinePort,'path':'/sample?from='+f.startSequence+'&interval=0&heartbeat=10000'},(r)=>{updateLoop(r);});
     }
 };

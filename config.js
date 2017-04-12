50c4e43e3dd5aaa0b7d22ebab05c69e46b0854ce
foxe2 <foxe2@rpi.edu>
2017-04-12 17:53:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = function(environment) {
             "upload": true
           },
           "file" : {
-            "dir": "C:/Users/Ethan/Downloads/BOXY_20170329.stpnc"
+            "dir": "./boxy.stpnc"
           },
           "machine" : {
             "dir" : "C:/Program Files (x86)/STEP Tools/STEP-NC Machine/machines/DMU_85.xml"
